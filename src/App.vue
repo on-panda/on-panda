d56dce2665eb3431c7227a994033706a78462fac
Lei <ylxx@live.com>
2024-09-14 22:07:43 +0800

    Refactor code to improve readability and maintainability

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup>
 import MessageMarkdown from './components/MessageMarkdown.vue'
 import { escapeHTML } from '@/utils/commonUtils'
-import { ref, computed } from 'vue'
+import { ref, computed, watch } from 'vue'
 import OpenAI from "openai";
 
 const openai = new OpenAI({
@@ -63,14 +63,19 @@ const patchs = computed(() => {
 //   return new TextDecoder('utf-8').decode(new Uint8Array(bytes))
 // })
 
-const probToColor = (prob) => {
-  // const color = `rgba(255, 32, 0, ${(1 - prob)})`;
-  // return { "background-color": color }
-  // const green = Math.floor((prob*0.8+.1) * 255)
+const probToColor = (prob, transparency) => {
   const green = Math.floor((prob) * (255 - 128))
-  const color = `rgb(${255 - green}, ${128 + green}, 128)`;
+  var rgb = `${255 - green}, ${128 + green}, 128`;
   // return {"box-shadow": "inset 0 -2px "+color}
-  return { "border-bottom": "3px solid " + color }
+  if ( 0 < prob && prob < 0.0001) {
+    rgb = "255, 0, 0"
+  }
+  if(transparency === undefined){
+    var color = `rgb(${rgb})`
+  }else{
+    var color = `rgba(${rgb}, ${transparency})`
+  }
+  return color
 }
 
 const p = (obj) => {
@@ -90,7 +95,7 @@ async function main() {
     messages: messages,
     stream: true,
     logprobs: true,
-    top_logprobs: 2,
+    top_logprobs: requestParams.value?.top_logprobs || 5,
     top_p: 1,
     // top_k: 2,
     // max_tokens: 54,
@@ -108,6 +113,10 @@ async function main() {
     }
   }
 }
+
+const requestParams = ref({
+  top_logprobs: 20
+})
 
 function createSpanInPatchSpanHTML(textContent) {
   const span = document.createElement('span')
@@ -154,26 +163,74 @@ const handleMouseEnterPatchSpan = (event) => {
   const patchIndex = event.target.attributes["patch-index"].value
   const patch = patchs.value[parseInt(patchIndex)]
 
-  patchTokens.value = patch.tokens
+  acitvatePatch.value = patch
   event.target.classList.add('ActivatePatchSpan')
   console.log(patchIndex, event.target.textContent, patch.tokens.length, patch.prob)
+
+  setFloatPatchPannelBelow(event.target)
+  
 }
 const handleMouseLeavePatchSpan = (event) => {
   // console.log(event)
   event.target.classList.remove('ActivatePatchSpan')
-}
+  floatPatchPannel.value.waitingToHide = true
+  setTimeout(() => {
+            if (floatPatchPannel.value.waitingToHide) {
+              floatPatchPannel.value.visible = false;
+              floatPatchPannel.value.waitingToHide = false;
+            }
+          }, 200);
+        }
 
 
 var messages = [{ role: "user", content: "just repeat `=🧎🏿‍♂️‍➡️磊<hr>\n蘒    𝒀𝒆𝒔`, no other words" }]
 // var messages = [{ role: "user", content:"just repeat `Yes,🧎🏿‍♂️‍➡️𝒀𝒆𝒔`, no other words" }]
 // var messages = [{ role: "user", content: "just output a random float64 without any words" }]
-// var messages = [{ role: "user", content: "用中文讲一个关于西游记的短笑话，不要有英文" }]
+var messages = [{ role: "user", content: "用中文讲一个关于西游记的短笑话，不要有英文" }]
+// var messages = [{ role: "user", content: "奥数题:已知小王 2024年30岁，本来预计60岁退休。但现在中央每五年开一次会，每开一次会决定退休年龄延迟3年，求老王的真正退休年龄。" }]
 
 main();
 
 
-// FloatPatchPannel
-const patchTokens = ref([])
+// floatPatchPannel
+const acitvatePatch = ref({})
+const acitvateLogprobItem = ref({})
+const tokenToHtml = (tokenContent) => {
+  if (tokenContent === undefined) {
+    return "undefined"
+  }
+  if (tokenContent === "") {
+    return "<|unsee|>"
+  }
+  return JSON.stringify(tokenContent)
+}
+
+
+const floatPatchPannel = ref({
+  visible: false,
+  waitingToHide: false,
+  x: 0,
+  y: 0,
+})
+
+watch(acitvatePatch, (newValue) => {
+    acitvateLogprobItem.value = {}
+});
+
+function setFloatPatchPannelBelow(element) {
+  const cellRect = element.getBoundingClientRect();
+  // const containerRect = $el.querySelector('.table-container').getBoundingClientRect();
+  floatPatchPannel.value.x = cellRect.left - 10
+  floatPatchPannel.value.y = cellRect.bottom - 5
+  floatPatchPannel.value.waitingToHide = false;
+  floatPatchPannel.value.visible = true;
+  console.log(element, cellRect, floatPatchPannel)
+}
+
+function continueFromToken(token, continuePrefix) {
+  continuePrefix = continuePrefix || ""
+  main()
+}
 
 </script>
 <template>
@@ -183,16 +240,38 @@ const patchTokens = ref([])
 
   <!-- <MessageMarkdown content="**Hello** _world_ $E=mc^2$!" /> -->
   <MessageMarkdown v-for="message in messages" :content="`### ${message['role']}:\n${message['content']}`" />
-  <h3> {{ tokens ? tokens[0].delta.role:"unknown_role"}}:</h3>
+  <h3> {{ tokens.length ? tokens[0].delta.role:"unknown_role"}}:</h3>
   <div style="background-color: #eee;white-space: pre-wrap;cursor: default;">
-    <span class="PatchSpan" v-for="patch in patchs" :style="probToColor(patch.prob)" :patch-index="patch.index"
+    <span class="PatchSpan" v-for="patch in patchs" :style='{ "border-bottom": "3px solid " + probToColor(patch.prob) }' :patch-index="patch.index"
       v-html="patchToSpanHTML(patch)" @mouseenter="handleMouseEnterPatchSpan"
       @mouseleave="handleMouseLeavePatchSpan"></span>
 
   </div>
   <hr>
-  <div class="FloatPatchPannel">
-    <div v-for="token in patchTokens" style="display: inline-block;">{{ token?.delta?.content }}</div>
+  <div class="floatPatchPannel" 
+  @mouseover="floatPatchPannel.waitingToHide = false"
+  :style="{
+            position: 'absolute',
+            left: `${floatPatchPannel.x}px`,
+            top: `${floatPatchPannel.y}px`,
+          }" v-if="floatPatchPannel.visible">
+    <div v-for="token in acitvatePatch.tokens.filter(token=>(token?.delta?.content !== undefined))" class="tokenPannel" style="vertical-align:top; display: inline-block; padding: 5px;">
+      <div class="tokenChosen" style="border-bottom: 2px solid #ccc;"v-html="escapeHTML(tokenToHtml(token?.delta?.content))">
+      </div>
+      <div class="tokenLogprobItems" >
+        <div v-for="logprobItem in token?.logprobs?.content[0].top_logprobs" style="display: block; background-color: #eee;" 
+        @click="continueFromToken(token, logprobItem.token)"
+        @mouseenter="acitvateLogprobItem=p(logprobItem)">
+          <span style="color: #888;">{{ tokenToHtml(logprobItem.token) }}</span>
+          <span :style='{ "background-color": probToColor(Math.exp(logprobItem.logprob), 0.18), "float": "right"}' style="white-space: pre-wrap;font-family: Monospace;" >:{{ (Math.exp(logprobItem.logprob)*100).toFixed(1).toString().padStart(5, ' ')}}%</span>
+        </div>
+      </div>
+    </div>
+    <footer>
+      <hr>
+      <button @click="floatPatchPannel.visible = false" style="padding: 0px; margin: 0 5px 5px 5px;"
+      >❌</button> <span v-if="acitvateLogprobItem.logprob" style="white-space: pre-wrap;font-family: Monospace;"> prob:{{ Math.exp(acitvateLogprobItem.logprob) }}</span>
+    </footer>
   </div>
 
 
@@ -206,6 +285,23 @@ const patchTokens = ref([])
 </template>
 
 <style scoped>
+
+.tokenChosen .tokenLogprobItems {
+  display: block;
+}
+
+.tokenPannel {
+  border: 1px solid #ccc;
+  padding: 0px;
+}
+.floatPatchPannel {
+  margin: 5px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  background-color: #f9f9f9;
+  position: absolute;
+}
+
 .PatchSpan {
   box-shadow: inset -1px 0 rgb(200, 200, 200);
   /* 使用 inset 将阴影设为内阴影 */
