852b958dc7671c12d4f7b06f6dbbb13b3d1c6699
Lei <ylxx@live.com>
2024-12-02 22:37:41 +0800

    add store

--- src/main.js
+++ src/main.js
@@ -1,14 +1,15 @@
 // import './assets/main.css'
 
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import '@/assets/main.css'
 import App from './App.vue'
 
+const pinia = createPinia()
 const app = createApp(App)
 
-
+app.use(pinia)
 app.use(ElementPlus)
-
 app.mount('#app')
