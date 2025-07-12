a69adde5740c46c2f5718693076ea408f9fed578
Lei <ylxx@live.com>
2025-07-12 13:40:41 +0800

    lazy load raw-json

--- src/components/widgets/ObjectViewerInDetails.vue
+++ src/components/widgets/ObjectViewerInDetails.vue
@@ -1,17 +1,17 @@
 <template>
-    <details>
+    <details ref="detailsRef" @toggle="handleToggle">
         <summary style="color: #606266">
-            <el-tooltip :content="props.tips" v-if="props.tips" placement="top">
+            <el-tooltip :content="props.tips" v-if="props.tips" placement="bottom">
                 <small>{{ props.summary }}</small>
             </el-tooltip>
             <small v-else>{{ props.summary }}</small>
         </summary>
-        <div class="raw-json-in-annotator-panel" style="overflow-x: scroll;">{{
-            JSON.stringify(props.object, null, 2) }}</div>
+        <div class="raw-json-in-annotator-panel" style="overflow-x: scroll;">{{ content }}</div>
     </details>
 </template>
 
 <script setup>
+import { ref } from 'vue'
 
 const props = defineProps({
     object: {
@@ -26,6 +26,14 @@ const props = defineProps({
         default: null
     },
 })
+
+const detailsRef = ref(null)
+// lazy load the content for better performance and avoid repeated key word find in the content
+const content = ref('PLACEHOLDER')
+
+const handleToggle = () => {
+    content.value = detailsRef.value?.open ? JSON.stringify(props.object, null, 2) : 'PLACEHOLDER'
+}
 
 </script>
 <style scoped>
